e3f1e29d126efa0c4b96a95ee1c2aeeac53c87f3
Davetechinnovation <davetechinnovation@gmail.com>
2025-09-13 16:56:57 +0100

    fix(portfolio): update Dandy Prime TV website link and remove development notice
    - Update the URL for the Dandy Prime TV project from dandy-prime-tv.vercel.app to dandy-prime-tv20.vercel.app
    - Remove the "Still under development" notice as the project might be completed or close to completion

--- portfolio/src/pages/Portfolio.jsx
+++ portfolio/src/pages/Portfolio.jsx
@@ -90,7 +90,7 @@ const Portfolio = () => {
           <img src={img5} alt="" className="w-full rounded-t-lg " />
           <div className="py-1 flex justify-center items-center ">
             <a
-              href="https://dandy-prime-tv.vercel.app/"
+              href="https://dandy-prime-tv20.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
               className="bg-[#444444] px-4 py-1 rounded-full flex items-center gap-1 hover:bg-transparent border-2 border-[#444444] duration-500 transition-all"
@@ -122,14 +122,7 @@ const Portfolio = () => {
               <ArrowUpRight />
             </a>
           </div>
-          <div className="flex justify-center items-center">
-            <p className="flex items-center  gap-3 text-yellow-400 ">
-              <span>
-                <AlertTriangle />
-              </span>{" "}
-              <span>Still under development</span>
-            </p>
-          </div>
+          
         </div>
       </div>
     </div>
